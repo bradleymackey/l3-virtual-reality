b1d1e93e8cf25ed3693f5fba24a06ba4472bcb98
Bradley Mackey <me@bradleymackey.com>
2019-02-19 20:22:49 +0000

    acc and mag data now normalised

--- data_corrector.py
+++ data_corrector.py
@@ -18,13 +18,25 @@ def get_sanitized_imu_data():
         next(reader) # skip header
         for row in reader:
             data.append(row)
-    print(f"> Successfully retrieved {len(data)} items from IMUData.csv.")
     data = np.array(data)
+    print(f"> Successfully retrieved {len(data)} items from IMUData.csv.")
     for row in range(len(data)):
         # rotational rate to radians
         for col in range(1,4):
-            data[row,col] = float(data[row,col]) * (np.pi/180)   
-    print("> Successfully normalized IMUData.csv data.")
+            data[row,col] = float(data[row,col]) * (np.pi/180) 
+        # normalize accelerometer
+        acc_range = range(4,7)
+        acc_len = np.linalg.norm(data[row,acc_range])
+        print("before:",data[row,acc_range])
+        for col in acc_range:
+            data[row,col] = 0.0 if acc_len==0.0 else float(data[row,col])/acc_len
+        print("after:",data[row,acc_range])
+        # normalize magnetometer
+        mag_range = range(7,10)
+        mag_len = np.linalg.norm(data[row,mag_range])
+        for col in mag_range:
+            data[row,col] = 0.0 if mag_len==0.0 else float(data[row,col])/mag_len
+    print("> Successfully normalized data.")
     return data 
 
 def main():
