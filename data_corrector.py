ab602dd34e16af9a19fa7d5acd5e31cd52fe8326
Bradley Mackey <me@bradleymackey.com>
2019-02-19 22:57:00 +0000

    added remainer of task 1 question

--- data_corrector.py
+++ data_corrector.py
@@ -9,6 +9,7 @@
 
 import csv
 import numpy as np
+import math
 
 def get_sanitized_imu_data():
     """retrieves the IMU data from the `IMUData.csv` file"""
@@ -38,24 +39,57 @@ def get_sanitized_imu_data():
     return data 
 
 def euler_to_qtrn(angles):
-    """converts a numpy euler angle array [a b c] to a quaternion array [i j k]"""
-    pass
+    """converts a numpy euler angle array [x y z] to a quaternion array [x y z w]"""
+    (yaw, pitch, roll) = (angles[0], angles[1], angles[2])
+    cy = np.cos(yaw * 0.5)
+    sy = np.sin(yaw * 0.5)
+    cp = np.cos(pitch * 0.5)
+    sp = np.sin(pitch * 0.5)
+    cr = np.cos(roll * 0.5)
+    sr = np.sin(roll * 0.5)
+    qw = cy * cp * cr + sy * sp * sr
+    qx = cy * cp * sr - sy * sp * cr
+    qy = sy * cp * sr + cy * sp * cr
+    qz = sy * cp * cr - cy * sp * sr
+    return np.array([qx, qy, qz, qw])
 
 def qtrn_to_euler(qtrn):
-    """converts a numpy quaternion array [i j k] to an euler angle array [a b c]"""
-    pass
+    """converts a numpy quaternion array [x y z w] to an euler angle array [x y z]"""
+    (x, y, z, w) = (qtrn[0], qtrn[1], qtrn[2], qtrn[3])
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll = math.atan2(t0, t1)
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch = math.asin(t2)
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw = math.atan2(t3, t4)
+    return np.array([yaw, pitch, roll])
 
 def qtrn_conj(qtrn):
-    """computes the conjugate of a quaternion, passed as a numpy array [i j k]"""
-    pass
+    """computes the conjugate of a quaternion, passed as a numpy array [x y z w]"""
+    (x, y, z, w) = (qtrn[0], qtrn[1], qtrn[2], qtrn[3])
+    return np.array([-x,-y,-z,w])
 
 def qtrn_mult(qtrn_1, qtrn_2):
-    """computes the product of 2 quaternions"""
-    pass
+    """computes the product of 2 quaternions, each [x y z w]"""
+    (a_x, a_y, a_z, a_w) = (qtrn_1[0], qtrn_1[1], qtrn_1[2], qtrn_1[3])
+    (b_x, b_y, b_z, b_w) = (qtrn_2[0], qtrn_2[1], qtrn_2[2], qtrn_2[3])
+    w = a_w * b_w - a_x * b_x - a_y * b_y - a_z * b_z
+    x = a_w * b_x + a_x * b_w + a_y * b_z - a_z * b_y
+    y = a_w * b_y - a_x * b_z + a_y * b_w + a_z * b_x
+    z = a_w * b_z + a_x * b_y - a_y * b_x + a_z * b_w
+    return np.array([x,y,z,w])
 
 
 def main():
     get_sanitized_imu_data()
+    q = euler_to_qtrn(np.array([0.2,1.2,2.2]))
+    print(q)
+    r = qtrn_to_euler(q)
+    print(r)
 
 if __name__=="__main__":
     main()
